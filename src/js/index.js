7cd96f024188f9fd7a41e5eb2fce3f816768eebd
cw.chen <sky85245630@gmail.com>
2019-11-07 18:02:31 +0800

    webpack include sass & babel

---
+++ src/js/index.js
@@ -0,0 +1,9 @@
+import '../css/main.scss'
+
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+console.log('hello world');
+
+const Name = ["louie",'parker','johnny','alan']
+Name.forEach((obj, idx)=> console.log(`${idx} => ${obj}`))
---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+var path = require('path');
+
+module.exports = {
+    context: path.resolve(__dirname, 'src'),
+    entry: './js/index.js',
+    output: {
+        path: path.resolve(__dirname,'dist/js'),
+        filename: 'index-bundle.js',
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js)$/,
+                exclude: /(node_modules)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                      presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+              test: /\.scss$/,
+              use: [
+                {
+                  loader: "style-loader" // creates style nodes from JS strings
+                },
+                {
+                  loader: "css-loader" // translates CSS into CommonJS
+                },
+                {
+                  loader: "sass-loader" // compiles Sass to CSS
+                }
+              ]
+            }
+        ]
+    }
+}
